03f25eb1ddf5cae1472468cef5b9d348a87c8e5b
Iago Oliveira Guedes <iagooliz@hotmail.com>
2024-06-08 12:50:32 -0300

    Organizando arquivos, criando ForEach para implementação do viewport simulando telas de celulares, criando estrutura da chamada na pasta fixtures, deixando o projeto mais organizado

--- cypress/e2e/CadastroUser.cy.js
+++ cypress/e2e/CadastroUser.cy.js
@@ -1,72 +1,83 @@
 /// <reference types="Cypress" />
 
 import { faker } from '@faker-js/faker';
+import screensData from '../fixtures/screens_mobile.json';
 
 
-describe('Cadastro de usuário', () => {
+const user_date = require('../fixtures/user_create.json')
+const { screens } = screensData;
 
-    const user_date = require('../fixtures/user_create.json')
 
-    beforeEach('Acessando página de cadastro', () => {
+screens.forEach(element => {
+    describe('Cadastro de usuário', () => {
 
-        //Acessar tela de cadastro de usuário
-        cy.accessRegisterPage()
-    })
+        beforeEach('Acessando página de cadastro', () => {
 
-    it('Validar campo nome vazio', () => {
+            //Verificando se o elemento for diferente de desktop, então chama o viewport com o tamanho da tela.
+            if (element != 'desktop') {
+                cy.viewport(element)
+            }
 
-        cy.SaveRegister()
-        cy.messageError('O campo nome deve ser prenchido')
+            //Acessar tela de cadastro de usuário
+            cy.accessRegisterPage()
+        })
+
+        it('Validar campo nome vazio', () => {
+
+            cy.SaveRegister()
+            cy.messageError('O campo nome deve ser prenchido')
+
+        });
+
+        it('Validar campo e-mail vazio', () => {
+
+            cy.fillName(user_date.name)
+            cy.SaveRegister()
+            cy.messageError('O campo e-mail deve ser prenchido corretamente')
+
+        });
+
+        it('Validar campo e-mail inválido', () => {
+
+            cy.fillName(user_date.name)
+            cy.fillEmail('emailinválido')
+            cy.SaveRegister()
+            cy.messageError('O campo e-mail deve ser prenchido corretamente')
+
+        });
+
+        it('Validar campo senha vazio', () => {
+
+            const name = faker.person.fullName()
+
+            cy.fillName(name)
+            cy.fillEmail(faker.internet.email(name))
+            cy.SaveRegister()
+            cy.messageError('O campo senha deve ter pelo menos 6 dígitos')
+
+        });
+
+        it('Validar campo senha inválido', () => {
+
+            cy.fillName(user_date.name)
+            cy.fillEmail(user_date.email)
+            cy.fillPassword('123')
+            cy.SaveRegister()
+            cy.messageError('O campo senha deve ter pelo menos 6 dígitos')
+
+        });
+
+        it('Validar campo senha com sucesso!', () => {
+
+            cy.fillName(user_date.name)
+            cy.fillEmail(user_date.email)
+            cy.fillPassword(user_date.password)
+            cy.SaveRegister()
+            cy.checkRegisterSucess(user_date.name)
+
+        });
+
 
     });
-
-    it('Validar campo e-mail vazio', () => {
-
-        cy.fillName(user_date.name)
-        cy.SaveRegister()
-        cy.messageError('O campo e-mail deve ser prenchido corretamente')
-
-    });
-
-    it('Validar campo e-mail inválido', () => {
-
-        cy.fillName(user_date.name)
-        cy.fillEmail('emailinválido')
-        cy.SaveRegister()
-        cy.messageError('O campo e-mail deve ser prenchido corretamente')
-
-    });
-
-    it('Validar campo senha vazio', () => {
-
-        const name = faker.person.fullName()
-
-        cy.fillName(name)
-        cy.fillEmail(faker.internet.email(name))
-        cy.SaveRegister()
-        cy.messageError('O campo senha deve ter pelo menos 6 dígitos')
-
-    });
-
-    it('Validar campo senha inválido', () => {
-
-        cy.fillName(user_date.name)
-        cy.fillEmail(user_date.email)
-        cy.fillPassword('123')
-        cy.SaveRegister()
-        cy.messageError('O campo senha deve ter pelo menos 6 dígitos')
-
-    });
-
-    it('Validar campo senha com sucesso!', () => {
-
-        cy.fillName(user_date.name)
-        cy.fillEmail(user_date.email)
-        cy.fillPassword(user_date.password)
-        cy.SaveRegister()
-        cy.checkRegisterSucess(user_date.name)
-
-    });
-
-
 });
+
